63947e2febccb4bee9cf8de0523b13ef40e727dc
Bailey Thompson <bailey.kyle.thompson@gmail.com>
2017-01-10 22:23:27 -0500

    Upgraded implicit multiplication and asymptotes.

--- GraphingCalculator.py
+++ GraphingCalculator.py
@@ -1,18 +1,29 @@
 # Bailey Thompson
-# Graphing Calculator (1.0.0)
-# 8 January 2017
+# Graphing Calculator (1.1.0)
+# 10 January 2017
 # Info: This programs is intended to graph functions.
 
 from tkinter import *
 from tkinter import ttk
 import math as m
 
-size = 8.0
-formula = "1/x"
-MAX_SIZE = 256.0
-MIN_SIZE = 0.25
+# size constants
+MAX_SIZE = 64.0
+MIN_SIZE = 1.0
 INCREMENT = 2.0
+
+# approximation constants
 COMPUTATION_DISTANCE = 0.001
+ASYMPTOTE = 2.0
+
+# global variables
+formula = "(x**3-2*x)/(2*(x**2-5))"
+size = 8.0
+
+
+def print_formula(pre_text):
+    Label(root, text=pre_text + "[{0:.0f}".format(size) + "] f(x) = " + formula, relief=RIDGE,
+          width=1).grid(row=1, column=0, columnspan=5, sticky=W + E)
 
 
 def translate(x, y):
@@ -29,6 +40,8 @@ def translate(x, y):
 def draw_line(xfrom, yfrom, xto, yto, colour):
     fromcoords = translate(xfrom, yfrom)
     tocoords = translate(xto, yto)
+    if yto - yfrom > size * ASYMPTOTE or yfrom - yto > size * ASYMPTOTE:
+        fromcoords = tocoords
     canvas.create_line(fromcoords[0], fromcoords[1], tocoords[0], tocoords[1], fill=colour)
 
 
@@ -50,28 +63,103 @@ def draw_graph(event):
             x = COMPUTATION_DISTANCE * size
             if eval(formula) < 0:
                 y *= -1
-        except SyntaxError:
-            Label(root, text="SYNTAX ERROR   [{0:.2f}".format(size) + "] f(x) = " + formula, relief=RIDGE,
-                  width=1).grid(row=1, columnspan=5,
-                                column=0,
-                                sticky=W + E)
-            break
-        except NameError:
-            Label(root, text="SYNTAX ERROR   [{0:.2f}".format(size) + "] f(x) = " + formula, relief=RIDGE,
-                  width=1).grid(row=1, columnspan=5,
-                                column=0,
-                                sticky=W + E)
+        except:
+            print_formula("SYNTAX ERROR   ")
             break
         try:
             draw_line(x - COMPUTATION_DISTANCE * size, yprev, x, y, "black")
         except:
-            Label(root, text="NON-INTEGER POWER   [{0:.2f}".format(size) + "] f(x) = " + formula, relief=RIDGE,
-                  width=1).grid(row=1, column=0,
-                                columnspan=5,
-                                sticky=W + E)
+            print_formula("NON-INT PWR (dbl click ^)   ")
             break
         yprev = y
         x += COMPUTATION_DISTANCE * size
+
+
+def append_formula(thing):
+    global formula
+    if formula.endswith('.') and thing == '.':
+        formula = formula[:-1]
+        formula += ","
+    else:
+        formula += thing
+    print_formula("")
+
+
+def clear_formula():
+    global formula
+    while formula != "":
+        delete_formula()
+    print_formula("")
+
+
+def delete_formula():
+    global formula
+    formula = formula[:-1]
+    print_formula("")
+
+
+def zoom_in():
+    global size, btnZoomIn, btnZoomOut
+    btnZoomOut = ttk.Button(root, text="Zoom Out", command=lambda: zoom_out()).grid(row=8, column=3)
+    if size > MIN_SIZE:
+        size /= INCREMENT
+        draw_graph("event")
+    if size == MIN_SIZE:
+        btnZoomIn = ttk.Button(root, text="Zoom In", command=lambda: zoom_in(), state=DISABLED).grid(row=8, column=2)
+    print_formula("")
+
+
+def zoom_out():
+    global size, btnZoomOut, btnZoomIn
+    btnZoomIn = ttk.Button(root, text="Zoom In", command=lambda: zoom_in()).grid(row=8, column=2)
+    if size < MAX_SIZE:
+        size *= INCREMENT
+        draw_graph("event")
+    if size == MAX_SIZE:
+        btnZoomOut = ttk.Button(root, text="Zoom Out", command=lambda: zoom_out(), state=DISABLED).grid(row=8, column=3)
+    print_formula("")
+
+
+def correct_ending_no_number(name):
+    return name.endswith('x') or name.endswith('e') or (name.endswith('i') and name[-2:] != "si") or name.endswith(')')
+
+
+def correct_ending(thing):
+    return thing[-1:].isdigit() or correct_ending_no_number(thing)
+
+
+def append_implicit(thing):
+    global formula
+    if correct_ending(formula):
+        if thing == "**":
+            formula += thing
+        else:
+            formula += "*" + thing
+    elif formula[-2:] == "**" and thing == "**":
+        formula = formula[:-2]
+        if correct_ending(formula):
+            formula += "*m.pow(x,"
+        else:
+            formula += "m.pow(x,"
+    else:
+        formula += thing
+    print_formula("")
+
+
+def append_number_formula(thing):
+    global formula
+    if correct_ending_no_number(formula) and thing.isdigit():
+        formula += "*"
+    formula += thing
+    print_formula("")
+
+
+def append_closing_parentheses_formula(thing):
+    global formula
+    if correct_ending(formula) and thing == '(':
+        formula += "*"
+    formula += thing
+    print_formula("")
 
 
 root = Tk()
@@ -85,90 +173,19 @@ root.geometry("+%d+%d" % (horizontalScreen, verticalScreen))
 
 canvas = Canvas(root)
 
+print_formula("")
 
-def print_formula():
-    Label(root, text="[{0:.2f}".format(size) + "] f(x) = " + formula, relief=RIDGE, width=1).grid(row=1, column=0,
-                                                                                                  columnspan=5,
-                                                                                                  sticky=W + E)
+btn0 = ttk.Button(root, text="0", command=lambda: append_number_formula("0")).grid(row=2, column=0)
+btn1 = ttk.Button(root, text="1", command=lambda: append_number_formula("1")).grid(row=2, column=1)
+btn2 = ttk.Button(root, text="2", command=lambda: append_number_formula("2")).grid(row=2, column=2)
+btn3 = ttk.Button(root, text="3", command=lambda: append_number_formula("3")).grid(row=2, column=3)
+btn4 = ttk.Button(root, text="4", command=lambda: append_number_formula("4")).grid(row=2, column=4)
 
-
-print_formula()
-
-
-def append_formula(thing):
-    global formula
-    if formula.endswith('.') and thing == '.':
-        formula = formula[:-1]
-        formula += ","
-    else:
-        formula += thing
-    print_formula()
-
-
-def clear_formula():
-    global formula
-    while formula != "":
-        delete_formula()
-    print_formula()
-
-
-def delete_formula():
-    global formula
-    formula = formula[:-1]
-    print_formula()
-
-
-def zoom_in():
-    global size, btnZoomIn, btnZoomOut
-    btnZoomOut = ttk.Button(root, text="Zoom Out", command=lambda: zoom_out()).grid(row=8, column=3)
-    if size > MIN_SIZE:
-        size /= INCREMENT
-        draw_graph("event")
-    if size == MIN_SIZE:
-        btnZoomIn = ttk.Button(root, text="Zoom In", command=lambda: zoom_in(), state=DISABLED).grid(row=8, column=2)
-    print_formula()
-
-
-def zoom_out():
-    global size, btnZoomOut, btnZoomIn
-    btnZoomIn = ttk.Button(root, text="Zoom In", command=lambda: zoom_in()).grid(row=8, column=2)
-    if size < MAX_SIZE:
-        size *= INCREMENT
-        draw_graph("event")
-    if size == MAX_SIZE:
-        btnZoomOut = ttk.Button(root, text="Zoom Out", command=lambda: zoom_out(), state=DISABLED).grid(row=8, column=3)
-    print_formula()
-
-
-def append_implicit(thing):
-    global formula
-    if formula[-1:].isdigit() or formula.endswith('e') or (formula.endswith('i') and formula[-2:] != "si"):
-        if thing == "**":
-            formula += thing
-        else:
-            formula += "*" + thing
-    elif formula[-2:] == "**":
-        formula = formula[:-2]
-        if formula[-1:].isdigit() or formula.endswith('e') or (formula.endswith('i') and formula[-2:] != "si"):
-            formula += "*m.pow(x,"
-        else:
-            formula += "m.pow(x,"
-    else:
-        formula += thing
-    print_formula()
-
-
-btn0 = ttk.Button(root, text="0", command=lambda: append_formula("0")).grid(row=2, column=0)
-btn1 = ttk.Button(root, text="1", command=lambda: append_formula("1")).grid(row=2, column=1)
-btn2 = ttk.Button(root, text="2", command=lambda: append_formula("2")).grid(row=2, column=2)
-btn3 = ttk.Button(root, text="3", command=lambda: append_formula("3")).grid(row=2, column=3)
-btn4 = ttk.Button(root, text="4", command=lambda: append_formula("4")).grid(row=2, column=4)
-
-btn5 = ttk.Button(root, text="5", command=lambda: append_formula("5")).grid(row=3, column=0)
-btn6 = ttk.Button(root, text="6", command=lambda: append_formula("6")).grid(row=3, column=1)
-btn7 = ttk.Button(root, text="7", command=lambda: append_formula("7")).grid(row=3, column=2)
-btn8 = ttk.Button(root, text="8", command=lambda: append_formula("8")).grid(row=3, column=3)
-btn9 = ttk.Button(root, text="9", command=lambda: append_formula("9")).grid(row=3, column=4)
+btn5 = ttk.Button(root, text="5", command=lambda: append_number_formula("5")).grid(row=3, column=0)
+btn6 = ttk.Button(root, text="6", command=lambda: append_number_formula("6")).grid(row=3, column=1)
+btn7 = ttk.Button(root, text="7", command=lambda: append_number_formula("7")).grid(row=3, column=2)
+btn8 = ttk.Button(root, text="8", command=lambda: append_number_formula("8")).grid(row=3, column=3)
+btn9 = ttk.Button(root, text="9", command=lambda: append_number_formula("9")).grid(row=3, column=4)
 
 btnSin = ttk.Button(root, text="sin", command=lambda: append_implicit("m.sin(")).grid(row=4, column=0)
 btnCos = ttk.Button(root, text="cos", command=lambda: append_implicit("m.cos(")).grid(row=4, column=1)
@@ -188,7 +205,7 @@ btnMultiply = ttk.Button(root, text="*", command=lambda: append_formula("*")).gr
 btnDivide = ttk.Button(root, text="/", command=lambda: append_formula("/")).grid(row=6, column=3)
 btnExponent = ttk.Button(root, text="^", command=lambda: append_implicit("**")).grid(row=6, column=4)
 
-btnLeftBracket = ttk.Button(root, text="(", command=lambda: append_formula("(")).grid(row=7, column=0)
+btnLeftBracket = ttk.Button(root, text="(", command=lambda: append_closing_parentheses_formula("(")).grid(row=7, column=0)
 btnRightBracket = ttk.Button(root, text=")", command=lambda: append_formula(")")).grid(row=7, column=1)
 btnPeriod = ttk.Button(root, text=".", command=lambda: append_formula(".")).grid(row=7, column=2)
 btnDelete = ttk.Button(root, text="Delete", command=lambda: delete_formula()).grid(row=7, column=3)
